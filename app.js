231364e08dcc870ee24b4226754b46418c017876
Nomin-Erdene624 <nomio.davka0624@gmail.com>
2024-09-22 17:15:24 +0800

    Add files via upload
    ugen togloom

---
+++ app.js
@@ -0,0 +1,93 @@
+// if,else->nuhtsul shalgah (tentsuu bnu,ih bnu,baga bnu)
+//>,<,==,!=,>=,<=
+var nas=18;
+if(nas>=18){
+	console.log("Nasand hursen");
+}else{
+	console.log("Nasand hureegui");
+}
+var une=30000;
+if(une>=50000){
+	console.log("unetei");
+}else{
+	console.log("hymd");
+}
+var too1=5, too2=10;
+if(too1>too2){
+	console.log(too1+" ih bna")
+}else{
+	console.log(too2+" ih bna")
+}
+var too=3;
+if(too>0){
+	console.log(too+" eyreg")
+}else{
+	console.log(too+" sorog")
+}
+var too3=5;
+if(too3%2==0){
+	console.log(too3+" sondgoi too");
+}else{
+	console.log(too3+" tegsh too")
+}
+// &&-> and/bas,||-> or/esvel,
+//35 5 bas 7-d zereg huvaagdah uu?
+var too5=30;
+if(too5%5==0 || too5%7==0){
+	console.log("huvaagdana");
+}else{
+	console.log("huvaagdahgui");
+}
+var age=4;
+if(age>=1 && age<3){
+	console.log("nylh")
+}else if (age>=3 && age<5){
+	console.log("balchir");
+}else{
+	
+}
+var num1=8, num2=6 , num3=7;
+if(num1>num2 && num1>num3  ){
+	console.log(num1+" ih bna");
+}else if (num2>num1 && num2>num3){
+	console.log(num2+" ih bna");
+}else{
+	
+}
+alert("Ta tanihgui oroond gantsaaraa serlee.");
+alert("Tom delgetsen deer bichleg yvj ehlev.");
+alert("Uchir bituuleg neg hun....");
+alert("Ene gazraas garahiin tuld 3 gazraar damjin yvah ystoi.");
+alert("Ehnii haalga:Holdoj uheh ayultai huiten gazar/Shataj uheh ayultai haluun nartai gazar" );
+var songolt1=prompt("1/2");
+	if(songolt1=="2"){
+	alert("Ta garj chadlaa.Oroi bolj nar jargah uyd yvj bolno");
+	alert("2dahi haalga:Hun chonotoi haalga/zombietei haalga");
+var songolt2=prompt("1/2")
+	if(songolt2=="1"){
+		alert("Ta davj garlaa.Sargui boloh uyd hun bolj huvirna");
+		alert("3n shoroon zam taarav.");
+		alert("tesreh bombogtei zam/mogoitoi zam/galtai zam");
+var songolt3=prompt("1/2/3")
+	if (songolt3=="3"){
+		alert("Ta garj chadav.Galtai zamiig shoroogoor untraasan.");
+		alert("Amjilttai gartee harilaa.");
+	}else if (songolt3=="2"){
+		alert ("Hordoj uhev.");
+	}else if (songolt3=="1"){
+		alert("Delberch uhev.");
+	}else{
+		alert("Tiim songolt baihgui.");
+	}
+	}else if (songolt2=="2"){
+		alert("Ta iduullee");
+	}else{
+		alert("Tiim songolt baihgui");
+	}
+	
+}else if (songolt1=="1"){
+	alert("Ta holdoj uhlee.");
+}else{
+	alert("Tiim songolt baihgui");
+}
+	
